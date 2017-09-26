a27cb13bf330253725c8f4afcd3ec4d2c9bcc8a1
Gaurav Sahni <kinglygaurav@gmail.com>
2017-09-26 15:28:35 -0500

    Implemented an Interace from Angular

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { IProduct } from './product';
 
 @Component({
@@ -6,8 +6,9 @@ import { IProduct } from './product';
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent{
-    pageTitle : string = 'Product List';
+export class ProductListComponent implements OnInit {
+    
+    pageTitle: string = 'Product List';
     imageWidth : number = 50;
     imageMargin : number = 2;
     showImage : boolean = false;
@@ -37,5 +38,9 @@ export class ProductListComponent{
 
     toggleImage() : void {
         this.showImage = !this.showImage;
+    }
+    
+    ngOnInit(): void {
+        console.log('In OnInit!!!');
     }
 }
