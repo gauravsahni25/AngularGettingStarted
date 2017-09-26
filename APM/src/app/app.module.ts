8f0a4bc12cd56eb5ceb41afa77858665bd608928
Gaurav Sahni <kinglygaurav@gmail.com>
2017-09-26 16:38:10 -0500

    Custom Pipe

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 //These are TS Imports
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
