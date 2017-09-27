df5eafb2d2be578512336de57c0bdc2eb66af785
Gaurav Sahni <kinglygaurav@gmail.com>
2017-09-26 19:37:34 -0500

    Hooked up Star Component to parent But wrong data Displayed as OnCHanges not fired

--- APM/src/app/shared/star.component.ts
+++ APM/src/app/shared/star.component.ts
@@ -9,6 +9,10 @@ export class StarComponent implements OnChanges{
     rating : number = 4;
     starWidth : number;
     
+    //This never occurs until we hook up the input from the Container Component, as we just 
+    //have default values.
+    //Also note that OnChanges watches changes only to input properties.
+    //And so far we do not have any input properties.
     ngOnChanges(): void {
         this.starWidth = this.starWidth * 86/5; //86 is the width of the div in the template of this component
     }
