f43bcbdc8fc587cd8558ca611e13b21f431e1c73
Gaurav Sahni <kinglygaurav@gmail.com>
2017-09-26 20:23:05 -0500

    Sending Data inside the Nested Component
    OnChanges work now and UI DIsplays well

--- APM/src/app/shared/star.component.ts
+++ APM/src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges } from '@angular/core'
+import { Component, OnChanges, Input } from '@angular/core'
 
 @Component({
     selector : 'pm-star',
@@ -6,14 +6,11 @@ import { Component, OnChanges } from '@angular/core'
     styleUrls : ['./star.component.css']
 })
 export class StarComponent implements OnChanges{
-    rating : number = 4;
+    @Input() rating : number;
     starWidth : number;
     
-    //This never occurs until we hook up the input from the Container Component, as we just 
-    //have default values.
     //Also note that OnChanges watches changes only to input properties.
-    //And so far we do not have any input properties.
     ngOnChanges(): void {
-        this.starWidth = this.starWidth * 86/5; //86 is the width of the div in the template of this component
+        this.starWidth = this.rating * 86/5; //86 is the width of the div in the template of this component
     }
 }
